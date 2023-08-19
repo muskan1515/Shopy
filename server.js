90a184f166109d17e1d31316c677f004fa333492
muskan1515 <67676328+muskan1515@users.noreply.github.com>
2023-08-19 22:44:28 +0530

    Updated

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
+import {fileURLToPath} from 'url';
 
 //configure env
 dotenv.config();
@@ -16,6 +17,8 @@ dotenv.config();
 connectDB();
 
 //rest object
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 const app = express();
 
 //middelwares
